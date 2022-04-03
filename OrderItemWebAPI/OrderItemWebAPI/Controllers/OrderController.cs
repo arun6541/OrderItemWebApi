a0b7ab6ff52748ca6d7135fd9848a69d325da506
Arun <arun.reddy6541@gmail.com>
2022-04-03 19:24:29 +0530

    added

---
+++ OrderItemWebAPI/OrderItemWebAPI/Controllers/OrderController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderItemWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+
+namespace OrderItemWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        
+        public Cart Post(int menuItemId)
+        {
+            using (var client=new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:15338/api/");
+                var response = client.GetAsync($"MenuItem/{menuItemId}");
+                response.Wait();
+                var res = response.Result;
+                if(res.IsSuccessStatusCode)
+                {
+                    var list = res.Content.ReadAsAsync<MenuItem>();
+                    list.Wait();
+                    var menuItem = list.Result;
+                    return new Cart(1, 1, menuItemId, menuItem.Name);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            
+        }
+    }
+}
---
+++ OrderItemWebAPI/OrderItemWebAPI/Models/Cart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderItemWebAPI.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int MenuItemId { get; set; }
+
+        public string MenuItemName { get; set; }
+
+        public Cart(int id,int userId,int menuItemId,string menuItemName)
+        {
+            this.Id = id;
+            this.UserId = userId;
+            this.MenuItemId = menuItemId;
+            this.MenuItemName = menuItemName;
+        }
+    }
+}
